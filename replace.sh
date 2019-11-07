aa9952bc357ec7bfce7f6a4d61ce24cc5c9e75ab
yuqiuz <zhou.yuqiu@hotmail.com>
2019-11-07 10:59:46 +0800

    add replace.sh

---
+++ replace.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+
+sed -i 's/ā/a1/g' ./dev.txt
+sed -i 's/á/a2/g' ./dev.txt
+sed -i 's/ǎ/a3/g' ./dev.txt
+sed -i 's/à/a4/g' ./dev.txt
+
+sed -i 's/ē/e1/g' ./dev.txt
+sed -i 's/é/e2/g' ./dev.txt
+sed -i 's/ě/e3/g' ./dev.txt
+sed -i 's/è/e4/g' ./dev.txt
+
+sed -i 's/ī/i1/g' ./dev.txt
+sed -i 's/í/i2/g' ./dev.txt
+sed -i 's/ǐ/i3/g' ./dev.txt
+sed -i 's/ì/i4/g' ./dev.txt
+
+sed -i 's/ō/o1/g' ./dev.txt
+sed -i 's/ó/o2/g' ./dev.txt
+sed -i 's/ǒ/o3/g' ./dev.txt
+sed -i 's/ò/o4/g' ./dev.txt
+
+sed -i 's/ū/u1/g' ./dev.txt
+sed -i 's/ú/u2/g' ./dev.txt
+sed -i 's/ǔ/u3/g' ./dev.txt
+sed -i 's/ù/u4/g' ./dev.txt
+
+sed -i 's/ü/v/g' ./dev.txt
+sed -i 's/ǘ/v2/g' ./dev.txt
+sed -i 's/ǚ/v3/g' ./dev.txt
+sed -i 's/ǜ/v4/g' ./dev.txt
